265508fe57001b719cefbc267116ea44a21e905c
Bernard Antwi <bernardantwi2010@gmail.com>
2021-12-13 00:54:34 +0000

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="text-center text-white bg-blue-800 p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">      
+      <i class="fa fa-viruses"></i>Covid-19 Tracker
+    </div>
+
+    <p>
+      Covid-19 API by
+      <a class="text-blue-500" href="covid19api.com" target="_blank"> covid19api.com</a>
+    </p>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
